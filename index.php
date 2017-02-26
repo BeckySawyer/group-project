161abad03af975573ccd7d81fe99c2b89fcbe41f
maryo222 <maryanneorton@gmail.com>
2017-02-27 08:39:15 +1300

    Linked index page to dynamic nav

--- index.php
+++ index.php
@@ -8,34 +8,9 @@
 <body>
 
 <body>
-    <div class="header-nightsky">
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <a class="navbar-brand" href="index.html">THE BEACH</a>
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="active"><a href="index.html">HOME</a></li>
-                        <li><a href="sand.html">SAND</a></li>
-                        <li><a href="waves.html">WAVES</a></li>
-                        <li><a href="birds.html">BIRDS</a></li>
-                        
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="hero">
-            <h1>Welcome to 'The Beach' website</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut tempor enim.</p>
-            <div class="btn btn-primary">Learn more</div>
-        </div>
-    </div>
+<?php 
+    include 'navigation.php';
+?>
 
 	<img src="img/flamingo.jpg">
 
--- navigation.php
+++ navigation.php
@@ -1,4 +1,28 @@
-<a href="/group-project"> Home</a>
-<a href="/group-project/sand.php"> Sand</a>
-<a href="/group-project/waves.php"> Waves</a>
-<a href="/group-project/birds.php"> Birds</a>
+    <div class="header-nightsky">
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">THE BEACH</a>
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="/group-project"> Home</a></li>
+						<li><a href="/group-project/sand.php"> Sand</a></li>
+						<li><a href="/group-project/waves.php"> Waves</a></li>
+						<li><a href="/group-project/birds.php"> Birds</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="hero">
+            <h1>Welcome to 'The Beach' website</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut tempor enim.</p>
+            <div class="btn btn-primary">Learn more</div>
+        </div>
+    </div>
