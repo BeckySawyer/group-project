b73c096c6be18789619a1e21f604e327a4172566
BeckySawyer <yougobeckycoco@Rebeccas-MBP.atcltd.atc.org.nz>
2017-02-27 09:33:01 +1300

    edited image

--- birds.php
+++ birds.php
@@ -13,7 +13,6 @@
 
 <div class="container">
   <h1>Birds</h1>
-  <img src="img/seagulls.jpg">
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
